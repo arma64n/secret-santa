3c3175ae3e5b4750564eaba9f4b8f262b8547616
Arman Abdrakhmanov <arma64n@gmail.com>
2020-01-14 22:27:56 +0600

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,52 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div v-for="(user, index) in users" :key="index">
+      {{ index + 1 }}<input type="text" v-model="user.name">
+    </div>
+    <pre>{{result}}</pre>
+    <button @click="addUser()">Add User</button>
+    <button @click="generateRandom()">Generate</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      users: [
+        {name: 'Frank'},
+        {name: 'John'},
+        {name: 'Didier'},
+        {name: 'Peter'}
+      ],
+      finalArray: [],
+      result: []
+    }
+  },
+  methods: {
+    addUser() {
+      this.users.push({name: ''})
+    },
+    generateRandom() {
+      this.finalArray = this.users.map(x => x.name)
+      for (let i of this.users) {
+        let receiver = this.randomize(i.name, this.finalArray)
+        this.finalArray = this.finalArray.filter(x => x !== receiver)
+      }
+    },
+    randomize (user) {
+      let number = Math.floor(Math.random() * this.finalArray.length)
+      if (this.finalArray[number] !== user) {
+        this.result.push({
+          santa: user,
+          receiver: this.finalArray[number]
+        })
+        return this.finalArray[number]
+      } else {
+        this.randomize(user)
+      }
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
